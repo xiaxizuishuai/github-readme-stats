78ec19872617259b7302e9ac0689d7ce29cbc27c
Jinyi Chen <188824655+xiaxizuishuai@users.noreply.github.com>
2025-03-23 21:18:15 +0800

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -73,7 +73,7 @@ export const themes = {
     title_color: "70a5fd",
     icon_color: "bf91f3",
     text_color: "38bdae",
-    bg_color: "1a1b27",
+    bg_color: "DEG，145,190,240",
   },
   onedark: {
     title_color: "e4bf7a",
